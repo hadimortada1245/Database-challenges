09b42613a585b6042b243305011921b83d7b113d
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-09-14 11:25:44 +0300

    Done with challenge2 in mongo

--- mongo/App.js
+++ mongo/App.js
@@ -1,12 +1,11 @@
-const http = require('http');
+const express= require('express');
+const app =express();
 
 const PORT = 5000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Server is running on port ' + PORT);
-});
-
-server.listen(PORT, () => {
+app.get('/api', (req, res) => {
+    res.send('Welcome to the API!');
+  });
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
