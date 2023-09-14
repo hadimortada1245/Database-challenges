0827e546f6373e52bb9561027e26995623e17f39
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-09-14 12:28:39 +0300

    Done with challenge 3 in mongo

--- mongo/App.js
+++ mongo/App.js
@@ -1,8 +1,8 @@
 const express= require('express');
+const mongoose=require('mongoose');
 const app =express();
-
 const PORT = 5000;
-
+mongoose.connect('mongodb+srv://hadimortada1245:LmIYlYUu3v8PAb5G@cluster0.708wnhp.mongodb.net/firstone');
 app.get('/api', (req, res) => {
     res.send('Welcome to the API!');
   });
