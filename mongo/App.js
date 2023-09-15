1e688c02e4f354590174e7fbde182f5cb6dbd9f7
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-09-15 11:10:26 +0300

    Done with this project

--- mongo/App.js
+++ mongo/App.js
@@ -1,11 +1,53 @@
-const express= require('express');
-const mongoose=require('mongoose');
-const app =express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
 const PORT = 5000;
-mongoose.connect('mongodb+srv://hadimortada1245:LmIYlYUu3v8PAb5G@cluster0.708wnhp.mongodb.net/firstone');
-app.get('/api', (req, res) => {
-    res.send('Welcome to the API!');
-  });
+mongoose.connect('mongodb+srv://hadimortada1245:LmIYlYUu3v8PAb5G@cluster0.708wnhp.mongodb.net/Hadi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const User = mongoose.model('User', {
+  username: String,
+  email: String,
+  age: Number,
+});
+app.use(bodyParser.json());
+const sampleUser = new User({
+  username: 'john_doe',
+  email: 'john@example.com',
+  age: 30,
+});
+
+sampleUser.save((err) => {
+  if (err) {
+    console.error('Error saving sample user:', err);
+  }
+});
+
+// Fetch all user data
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+app.get('/api/users/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
